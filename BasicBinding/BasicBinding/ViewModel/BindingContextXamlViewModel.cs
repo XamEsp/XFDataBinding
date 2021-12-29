bf271be3a94c7d4b4fb727482d166d62753eb4be
Rodrigo Juarez <rodrigomjuarez@gmail.com>
2021-12-29 16:32:39 -0300

    Basic binding context WIP

---
+++ BasicBinding/BasicBinding/ViewModel/BindingContextXamlViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MvvmHelpers;
+using System;
+
+namespace BasicBinding.ViewModel
+{
+    public class BindingContextXamlViewModel : BaseViewModel
+    {
+        public BindingContextXamlViewModel()
+        {
+            Now = DateTime.Now;
+        }
+
+        DateTime now;
+
+        public DateTime Now
+        {
+            get => now;
+            set => SetProperty(ref now, value);
+        }
+
+    }
+}
---
+++ BasicBinding/BasicBinding/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using BasicBinding.View;
+using MvvmHelpers;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BasicBinding.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+        Command bindingContextXamlCommand;
+
+        public ICommand BindingContextXamlCommand
+        {
+            get
+            {
+                if (bindingContextXamlCommand == null)
+                {
+                    bindingContextXamlCommand = new Command(BindingContextXaml);
+                }
+
+                return bindingContextXamlCommand;
+            }
+        }
+
+        async void BindingContextXaml()
+        {
+            await App.Navigation.PushAsync(new BindingContextXamlPage());
+        }
+    }
+}
