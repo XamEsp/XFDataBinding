405a371d282277216ffa6d939f8173caff642240
Jesse Liberty <jliberty@agdna.com>
2022-02-01 08:49:25 -0500

    Advanced Databinding Part 0 - Initial checkin

---
+++ AdvancedDatabindingPart0/AdvancedDatabindingPart0/AdvancedDatabindingPart0/Model/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedDatabindingPart0.Model
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+    }
+
+    public class BookStore
+    {
+        public string StoreName { get; set; }
+        public List<Book> Books { get; set; }
+        public BookStore()
+        {
+            StoreName="Jesse's Books";
+            var booksInStock = new List<Book>
+        {
+           new Book
+           {
+                Name="The Sound And The Fury",
+                Author="Faulkner",
+           },
+            new Book
+            {
+                Name = "The Time Of Our Singing",
+                Author = "Powers",
+            },
+
+
+            new Book
+            {
+                Name= "In Search of Lost Time",
+                Author="Proust",
+
+            }
+
+        };
+
+            Books=booksInStock;
+        }
+    }
+
+
+}
